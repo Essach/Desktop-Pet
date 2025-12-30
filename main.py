a8d310d3570f6ad0a4824c837be964f30b8012df
Essach <adrian.wozny@interia.pl>
2025-12-30 18:14:16 +0100

    Feat. moving func, drag and drop

--- main.py
+++ main.py
@@ -1,82 +1,78 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QMenu
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QMenu, QDesktopWidget
 from PyQt5.QtGui import QContextMenuEvent
-from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtCore import Qt, QTimer, QPoint
 
 class DesktopPet(QMainWindow):
     def __init__(self, screenSize):
         super().__init__()
         self.setAttribute(Qt.WA_TranslucentBackground, True)
         self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setWindowFlag(Qt.WindowStaysOnTopHint)
+
+        self.screenWidth = screenSize.width()
+        self.screenHeight = screenSize.height()
         # self.setGeometry(screenSize.width() - 150,
                         # screenSize.height() - 200,
                         # 150, 150)
-        self.posX = 800
-        self.posY = 500
+        # self.posX = 800
+        # self.posY = 500
 
-        self.setGeometry(self.posX, self.posY, 150, 150)
+        self.setFixedSize(150, 150)
+        self.center()
+        self.oldPos = self.pos()
 
-        self.char = QPushButton("", self)
+        # self.setGeometry(self.posX, self.posY, 150, 150)
+
+        self.char = QLabel(self)
         
         self.initUI()
         
         
     def initUI(self):
         self.char.setGeometry(0,0,150,150)
-        self.char.setStyleSheet("border-image: url(saka.png);")
+        self.char.setStyleSheet("border-image: url(./saka.png);")
+        self.char.setCursor(Qt.PointingHandCursor)
         
-        self.char.clicked.connect(self.click_char)
+        # self.char.clicked.connect(self.click_char)
 
     def click_char(self):
         print("Hallo!")
-        self.move('right', 300)
+        # self.walk('left', 300)
 
-    def move(self, direction, distance):
+    def walk(self, direction, distance):
         self.starting_distance = distance
-        self.steps_left = distance
         self.current_direction = direction
+        self._walkCheck()
+        self.steps_left = self.starting_distance
 
+        self._walkAnimate()
         self.timer = QTimer(self)
-        self.timer.timeout.connect(self._moveStep)
+        self.timer.timeout.connect(self._walkStep)
         self.timer.start(1)
-        
-        # if direction == "right" or direction == "left":
-        #     self.timer.start(10)
-        # elif direction == "down":
-            # for _ in range(distance//10):
-            #     self.posY += 1
-            #     self.setGeometry(self.posX, self.posY, 150, 150)
-            # for _ in range((distance//20)*3):
-            #     self.posY += 2
-            #     self.setGeometry(self.posX, self.posY, 150, 150)
-            # for _ in range(((distance//40)*6)):
-            #     self.posY += 4
-            #     self.setGeometry(self.posX, self.posY, 150, 150)
-
-            # self.timer.start(10)
-            # self.timer.start(1)
-            # phase1 = self.steps_left//10
-            # phase2 = (3*self.steps_left)//10
-            # phase3 = (6*self.steps_left)//10
-            # print(phase1, phase2, phase3)
-
-            # self.steps_left = phase1
-            # self.timer.start(8)
-            # self.steps_left = phase2
-            # self.timer.start(4)
-            # self.steps_left = phase3
-            # self.timer.start(2)
-            # pass
-            
-        # elif direction == "up":
-            # for _ in range(distance):
-            #     self.posY -= 1
-            #     self.setGeometry(self.posX, self.posY, 150, 150)
-        
     
-    def _moveStep(self):
+    def _walkAnimate(self):
+        if self.current_direction == "stand":
+            self.char.setStyleSheet("border-image: url(saka.png)")
+        elif self.current_direction == "down" or self.current_direction == "up":
+            self.char.setStyleSheet("border-image: url(./sakaback.png);")
+
+    def _walkCheck(self):
+        if self.current_direction == "down" and self.starting_distance > (self.screenHeight-50)-(self.posY+150):
+            self.starting_distance = (self.screenHeight-50)-(self.posY+150)
+        elif self.current_direction == "up" and self.posY - self.starting_distance < 0:
+            self.starting_distance = self.posY
+        elif self.current_direction == "right" and self.starting_distance > self.screenWidth-(self.posX+150):
+            self.starting_distance = self.screenWidth-(self.posX+150)
+        elif self.current_direction == "left" and self.posX - self.starting_distance < 0:
+            self.starting_distance = self.posX
+
+    def _walkStep(self):
+        # print(self.posX, self.posY)
         if self.steps_left <= 0:
             self.timer.stop()
+            self.current_direction = "stand"
+            self._walkAnimate()
             return
 
         if self.current_direction == "right":
@@ -89,15 +85,32 @@ class DesktopPet(QMainWindow):
             if self.steps_left > 0.9*self.starting_distance:
                 self.posY += 1
             elif self.steps_left > 0.6*self.starting_distance:
-                self.posY += 3
-                self.steps_left -= 2
+                self.posY += 2
+                self.steps_left -= 1
             else: 
-                self.posY += 6
-                self.steps_left -= 5
+                self.posY += 4
+                self.steps_left -= 3
         
         self.setGeometry((int(self.posX)), int(self.posY), 150, 150)
         self.steps_left -= 1
-                
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def mousePressEvent(self, event):
+        self.oldPos = event.globalPos()
+
+    def mouseMoveEvent(self, event):
+        self.char.setStyleSheet("border-image: url(./sakaback.png);")
+        delta = QPoint(event.globalPos() - self.oldPos)
+        self.move(self.x() + delta.x(), self.y() + delta.y())
+        self.oldPos = event.globalPos()
+
+    def mouseReleaseEvent(self, event):
+        self.char.setStyleSheet("border-image: url(./saka.png);")
 
     def contextMenuEvent(self, event: QContextMenuEvent):
         contextMenu = QMenu(self)
